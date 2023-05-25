f856a7d64f6bd18d87efa790ebb5379a1a45d97f
Edu Olivares <116183221+titodelas@users.noreply.github.com>
2023-05-25 14:30:29 +0200

    WORKING

--- bot/main.py
+++ bot/main.py
@@ -7,29 +7,33 @@ import random
 TOKEN = 'MTA4NzA3MDk5NDU2MjI5ODAwNg.GAV8SV.ydnomqsVQa-pICYTaJEHsQq15dlth3zKF28dzs'
 
 # Configura la lista de IDs de servidores donde se seleccionarán los usuarios aleatorios
-servidores_ids = [1111023786276442202]  # IDs de los servidores
+servidores_ids = []  # IDs de los servidores
 
 # Configura la lista de IDs de canales de anuncios
 # IDs de los canales para los anuncios
-canales_ids_anuncio = [1111039972443693136]
+canales_ids_anuncio = [1088487226628919326, 1088487283717586994, 1088487807326105721,
+                       1077521551815102514, 1077521918443388998, 1077521987536162876, 949421796917133372,
+                       1090227243017572373, 1090227291981889606, 1090227324756168756,
+                       682102282548150373, 661681250783985694, 661681300360396810,
+                       1109876895182491700, 1109877039567224892, 1109877110371270737]
 
 # Configura el intervalo de tiempo entre cada anuncio (en segundos)
-intervalo_anuncio = 30  # 1 hora = 3600 segundos
+intervalo_anuncio = 600  # 1 hora = 3600 segundos
 
 # Configura las rutas de las imágenes que deseas adjuntar en los anuncios
 rutas_imagenes = ['150.jpg', '350.jpg',
                   '900.jpg']  # Rutas de las imágenes
 
 # Configura el intervalo de tiempo entre cada mensaje directo (en segundos)
-intervalo_dm = 30  # 1.5 horas = 5400 segundos
+intervalo_dm = 5400  # 1.5 horas = 5400 segundos
 
-# intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!') # intents=intents
+bot = commands.Bot(command_prefix='$')
 
 
 @bot.event
 async def on_ready():
-    print('Bot presence t u r n e d on ( ͡° ͜ʖ ͡°)')
+    print('El bot está listo')
+    enviar_anuncio.start()
 
 
 @bot.command()
@@ -45,7 +49,6 @@ async def ayuda(ctx):
 
     await ctx.author.send(embed=embed)
 
-
 @tasks.loop(seconds=intervalo_anuncio)
 async def enviar_anuncio():
     for canal_id in canales_ids_anuncio:
@@ -56,19 +59,7 @@ async def enviar_anuncio():
             with open(ruta_imagen, 'rb') as imagen:
                 imagenes_adjuntas.append(discord.File(imagen))
 
-        await canal_anuncio.send(files=imagenes_adjuntas)
-
-
-@tasks.loop(seconds=intervalo_dm)
-async def enviar_dm():
-    for servidor_id in servidores_ids:
-        servidor = bot.get_guild(servidor_id)
-
-        if servidor is not None:
-            usuarios = servidor.members
-
-            if len(usuarios) > 0:
-                mensaje = """
+        await canal_anuncio.send('''
 **𝐏𝐚𝐲𝐏𝐚𝐥 𝐀𝐜𝐜𝐨𝐮𝐧𝐭𝐬**
 ```yaml
 🔒 150€ Balance ➢ 10€
@@ -81,44 +72,19 @@ async def enviar_dm():
 ```
 :shopping_cart: **Payment Method:** Cryptocurrencies (*ETH, BTC, SOL, etc...*) only.
 
-> **__Purchase here:__ @Ace Transfers#9106  (DM)**
+> **__Purchase here:__ <@1087070994562298006>  (DM)**
 > **FAQ: If you have questions drop a message.**
 > **Vouches: You can ask for reviews at my DM, also have proofs & can __screenshare__.**
-        """
-                imagenes_adjuntas = []
-
-                for ruta_imagen in rutas_imagenes:
-                    with open(ruta_imagen, 'rb') as imagen:
-                        imagenes_adjuntas.append(discord.File(imagen))
-
-                while len(usuarios) > 0:
-                    usuario = random.choice(usuarios)
-                    try:
-                        await usuario.send(content=mensaje, files=imagenes_adjuntas)
-                        break  # Sale del bucle si el mensaje se envió correctamente
-                    except discord.Forbidden:
-                        # Remueve al usuario de la lista si los DM están cerrados
-                        usuarios.remove(usuario)
-
+        ''', files=imagenes_adjuntas)
 
 @bot.command()
 async def start(ctx):
     enviar_anuncio.start()
-    enviar_dm.start()
-
-    embed = discord.Embed(
-        title="Bot iniciado", description="El envío de anuncios y mensajes directos ha comenzado.", color=discord.Color.green())
-    await ctx.author.send(embed=embed)
-
+    await ctx.send('El envío de anuncios y mensajes directos ha comenzado')
 
 @bot.command()
 async def stop(ctx):
     enviar_anuncio.cancel()
-    enviar_dm.cancel()
+    await ctx.send('El envío de anuncios y mensajes directos ha sido detenido')
 
-    embed = discord.Embed(
-        title="Bot detenido", description="El envío de anuncios y mensajes directos ha sido detenido.", color=discord.Color.red())
-    await ctx.author.send(embed=embed)
-
-# https://www.reddit.com/r/Discord_selfbots/comments/n7wo3u/creating_a_discord_self_bot/
 bot.run(TOKEN, bot=False)
